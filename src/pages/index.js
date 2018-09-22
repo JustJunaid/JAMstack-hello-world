4b711fb69cf8047f5c8eea5e81de86ffcd68bdfa
JUNAID CHOUDHARY <junaidchoudhary210@gmail.com>
2018-09-22 23:50:34 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,78 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-      <h1>Junaid's Image Gallery</h1>
-
-    <h1>Photo Gallery App</h1>
-    <p>Welcome to the Image Gallery</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div style={{
+    display: 'flex',
+    flexWrap: 'wrap',
+  }}>
+    <div style={{
+      flex: '33.33%',
+      padding: '1rem',
+    }}>
+      <img style={{border:'1px solid #c3c3c3', borderRadius: '2rem'}} src="https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&h=350"></img>
+    </div>
+    <div style={{
+      flex: '33.33%',
+      padding: '1rem',
+    }}>
+      <img style={{border:'1px solid #c3c3c3', borderRadius: '2rem'}} src="https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&h=350"></img>
+    </div>
+    <div style={{
+      flex: '33.33%',
+      padding: '1rem',
+    }}>
+      <img style={{border:'1px solid #c3c3c3', borderRadius: '2rem'}} src="https://images.pexels.com/photos/57416/cat-sweet-kitty-animals-57416.jpeg?auto=compress&cs=tinysrgb&h=350"></img>
+    </div>
+    
+  </div>
+  <div style={{
+    display: 'flex',
+    flexWrap: 'wrap',
+  }}>
+    <div style={{
+      flex: '33.33%',
+      padding: '1rem',
+    }}>
+      <img style={{border:'1px solid #c3c3c3', borderRadius: '2rem'}} src="https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&h=350"></img>
+    </div>
+    <div style={{
+      flex: '33.33%',
+      padding: '1rem',
+    }}>
+      <img style={{border:'1px solid #c3c3c3', borderRadius: '2rem'}} src="https://images.pexels.com/photos/209037/pexels-photo-209037.jpeg?auto=compress&cs=tinysrgb&h=350"></img>
+    </div>
+    <div style={{
+      flex: '33.33%',
+      padding: '1rem',
+    }}>
+      <img style={{border:'1px solid #c3c3c3', borderRadius: '2rem'}} src="https://images.pexels.com/photos/57416/cat-sweet-kitty-animals-57416.jpeg?auto=compress&cs=tinysrgb&h=350"></img>
+    </div>
+    
+  </div>
+  <div style={{
+    display: 'flex',
+    flexWrap: 'wrap',
+  }}>
+    <div style={{
+      flex: '33.33%',
+      padding: '1rem',
+    }}>
+      <img style={{border:'1px solid #c3c3c3', borderRadius: '2rem'}} src="https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&h=350"></img>
+    </div>
+    <div style={{
+      flex: '33.33%',
+      padding: '1rem',
+    }}>
+      <img style={{border:'1px solid #c3c3c3', borderRadius: '2rem'}} src="https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&h=350"></img>
+    </div>
+    <div style={{
+      flex: '33.33%',
+      padding: '1rem',
+    }}>
+      <img style={{border:'1px solid #c3c3c3', borderRadius: '2rem'}} src="https://images.pexels.com/photos/57416/cat-sweet-kitty-animals-57416.jpeg?auto=compress&cs=tinysrgb&h=350"></img>
+    </div>
+    
+  </div>
   </div>
 )
 
