42ca48398c3b13e6defc01f50d21b1f40ff96df6
JUNAID CHOUDHARY <junaidchoudhary210@gmail.com>
2018-09-22 10:38:37 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,9 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-<<<<<<< HEAD
-    <h1>Junaid's Image Gallery</h1>
-=======
+      <h1>Junaid's Image Gallery</h1>
+
     <h1>Photo Gallery App</h1>
->>>>>>> fc7d20d1e3f76a618714023523b09f17e7ddbf59
     <p>Welcome to the Image Gallery</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
